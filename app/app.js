556c007b617ab9b5906f7d1a2c7d78346ad3c35c
haronYunis <haronyunis@icloud.com>
2018-01-17 11:25:29 -0800

    fixed math bug and also added totals and averages.

--- app/app.js
+++ app/app.js
@@ -8,58 +8,69 @@ var trEl = document.createElement('tr');
 
 var storeHolder = [];
 
-function CookieStores(name, minCustomer, maxCustomer, minCookie, maxCookie,) {
+function CookieStores(name, minCustomer, maxCustomer, minCookie, maxCookie, avgCookie) {
   this.name = name;
   this.minCustomer = minCustomer;
   this.maxCustomer = maxCustomer;
   this.minCookie = minCookie;
   this.maxCookie = maxCookie;
+  this.avgCookie = avgCookie;
 
- }
+}
 
- CookieStores.prototype.render = function() {
-   var trEl = document.createElement('tr');
-   for (var i = 0; i < timeSlots.length; i++) { 
-    
-    var customers =  Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
+CookieStores.prototype.render = function () {
+  var trEl = document.createElement('tr');
+  for (var i = 0; i < timeSlots.length; i++) {
+
+    var customers = Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer * this.avgCookie;
     customers = Math.floor(customers);
     totalcustomers += customers;
-  
-    
-    var cookiesSold = Math.random() * (this.maxCookie - this.minCookie) + this.minCookie;
+
+
+    var cookiesSold = Math.random() * (this.maxCookie - this.minCookie) + this.minCookie * this.avgCookie;
     cookiesSold = Math.floor(cookiesSold);
     totalcookies += cookiesSold;
-    
+
 
     var tdEl = document.createElement('td');
     tdEl.textContent = this.name + customers + ' customers & ' + cookiesSold + ' cookies sold.';
-  
+
     trEl.appendChild(tdEl);
     tableId.appendChild(trEl);
-   }
-   var tdEl = document.createElement('td');
-   tdEl.textContent = totalcustomers + ' total customers today and ' + totalcookies + ' cookies sold.';
-   trEl.appendChild(tdEl);
-   tableId.appendChild(trEl);
- }
+  }
+  var tdEl = document.createElement('td');
+  tdEl.textContent = totalcustomers + ' total customers today and ' + totalcookies + ' cookies sold.';
+  trEl.appendChild(tdEl);
+  tableId.appendChild(trEl);
+  
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.avgCookie;
+  trEl.appendChild(tdEl);
+  tableId.appendChild(trEl);
+}
 
- function makeHeaderRow() {
+function makeHeaderRow() {
   var trEl = document.createElement('tr');
-   for(var i=0; i < timeSlots.length; i++) {
-     var thEl = document.createElement('th');
-     thEl.textContent = timeSlots[i];
-     trEl.appendChild(thEl);
-     tableId.appendChild(trEl);
-   } 
-   var thEl = document.createElement('th');
-   thEl.textContent = 'Totals';
-   trEl.appendChild(thEl);
- }
- var pike = new CookieStores('Pike Store: ',22,44,22,70);
- var seatac = new CookieStores('Seatac Store: ', 15, 33, 30, 40);
- var seattleCenter = new CookieStores('Seattle Center: ', 25, 75, 25, 105);
- var capitalHill = new CookieStores('Capitol Hill: ', 20, 55, 20, 40);
- var alki = new CookieStores('Alki: ', 40, 65, 40, 75);
+  for (var i = 0; i < timeSlots.length; i++) {
+    var thEl = document.createElement('th');
+    thEl.textContent = timeSlots[i];
+    trEl.appendChild(thEl);
+  }
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Totals';
+  trEl.appendChild(thEl);
+
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Average Cookies';
+  trEl.appendChild(thEl);
+
+  tableId.appendChild(trEl);
+}
+var pike = new CookieStores('Pike Store:', 500, 180, 180, 270, 6.3);
+var seatac = new CookieStores('Seatac Store:', 15, 33, 30, 40, 1.2);
+var seattleCenter = new CookieStores('Seattle Center:', 25, 75, 25, 105, 3.7);
+var capitalHill = new CookieStores('Capitol Hill:', 20, 55, 20, 40, 2.3);
+var alki = new CookieStores('Alki:', 40, 65, 40, 75, 4.6);
 
 makeHeaderRow();
 pike.render();
